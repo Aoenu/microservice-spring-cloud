8287afe6bec530dba369efcdfdb72e7e502273f1
aoenu <aoenu@aoenudeMacBook-Pro.local>
2018-10-30 16:00:54 +0800

    add

---
+++ sleuth-trace-two/src/main/java/com/hand/sleuth/SleuthTraceTwoApplication.java
@@ -0,0 +1,28 @@
+package com.hand.sleuth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class SleuthTraceTwoApplication {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/trace-2", method = RequestMethod.GET)
+    public String trace() {
+        logger.info("===<call trace-2>===");
+        return "Trace";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SleuthTraceTwoApplication.class, args);
+    }
+}
---
+++ spring-cloud-sleuth-zipkin/spring-cloud-producer/src/main/java/com/hand/producer/SpringCloudProducerApplication.java
@@ -0,0 +1,29 @@
+package com.hand.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class SpringCloudProducerApplication {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/trace-2", method = RequestMethod.GET)
+    public String trace() {
+        logger.info("===<call trace-2>===");
+        return "Trace";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudProducerApplication.class, args);
+    }
+}
