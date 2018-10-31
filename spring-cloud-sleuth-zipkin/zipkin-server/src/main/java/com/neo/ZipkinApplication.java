ef5ca77c586054f89fec48e7361ad8d6378e11b3
aoenu <aoenu@aoenudeMacBook-Pro.local>
2018-10-31 13:58:40 +0800

    add

--- spring-cloud-sleuth-zipkin/zipkin-server/src/main/java/com/neo/ZipkinApplication.java
+++ spring-cloud-sleuth-zipkin/zipkin-server/src/main/java/com/neo/ZipkinApplication.java
@@ -3,11 +3,13 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZipkinServer
+//@EnableZipkinServer
+@EnableZipkinStreamServer
 public class ZipkinApplication {
 
 	public static void main(String[] args) {
